270ac255fb6814a5db9c50a0c96b981f38a8b19b
无为 <3435516833@qq.com>
2023-01-30 19:07:20 +0800

    增加自动化发布

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+ 
+# 忽略错误
+set -e  #有错误抛出错误
+ 
+# 构建
+yarn run docs:build  #然后执行打包命令
+ 
+# 进入待发布的目录
+cd docs/.vitepress/dist  #进到dist目录
+ 
+git init  #执行这些git命令
+git add -A
+git commit -m 'deploy'
+ 
+git push -f https://github.com/18262311226/pied-ui.git master:gh-pages  #提交到这个分支
+ 
+cd -
+ 
+rm -rf docs/.vitepress/dist  #删除dist文件夹
