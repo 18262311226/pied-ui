c5b209e6cb7a90d6d6a1d2955e33401feaf6b023
无为 <3435516833@qq.com>
2023-01-30 19:00:26 +0800

    更改文档路径

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,8 @@
 // docs/.vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme'
-import PiedUi from 'pied-design-ui'
-import '../../../node_modules/pied-design-ui/dist/index.css'
+// import PiedUi from 'pied-design-ui'
+// import '../../../node_modules/pied-design-ui/dist/index.css'
+import PiedUi from '../../../components/index.js'
 
 export default {
   ...DefaultTheme,
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import './style/style.scss'
 import App from './App.vue'
-import PiedUi from 'pied-design-ui'
-import '../node_modules/pied-design-ui/dist/index.css'
+import PiedUi from '../components/index'
+// import PiedUi from 'pied-design-ui'
+// import '../node_modules/pied-design-ui/dist/index.css'
 
 createApp(App).use(PiedUi).mount('#app')
