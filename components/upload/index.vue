0cdbbf0d919d63f5cecd5d47f5907b3284e157da
Gerfory <3435516833@qq.com>
2023-02-10 17:14:23 +0800

    增加组件

--- components/upload/index.vue
+++ components/upload/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>{{ files }}</div>
-  <button type="button" @click="open">Choose file</button>
+<div class="pied-upload">
+  <div class="upload-source">
+    <input type="file" ref="file" @change="changeFile">
+  </div>
+  <div class="upload-button" @click="open" @mouseover="onMouseover" @mouseleave="onMouseleave">
+    上传文件
+  </div>
+  <transition name="fade">
+    <div v-if="bgShow" class="button-bg"></div>
+  </transition>
+</div>
 </template>
 
 <script>
@@ -10,13 +19,58 @@ export default {
 </script>
 
 <script setup>
-import { useFileDialog } from '@vueuse/core'
+import { ref } from 'vue'
+const file = ref('')
+const bgShow = ref(false)
+const open = () => {
+  file.value.dispatchEvent(new MouseEvent('click')) 
+}
 
-const { files, open, reset } = useFileDialog()
+const onMouseover = () => {
+  bgShow.value = true
+}
 
-console.log(files)
+const onMouseleave = () => {
+  bgShow.value = false
+}
+
+const changeFile = (value) => {
+  console.log(value.target.files)
+}
 </script>
 
 <style lang="scss" scoped>
+.pied-upload{
+  width:100%;
+  position: relative;
+  .upload-source{
+    display: none;
+  }
+  .upload-button{
+    width:100%;
+    height:50px;
+    border:2px solid purple;
+    text-align: center;
+    line-height: 45px;
+    cursor: pointer;
+    background: transparent;
+  }
+  .button-bg{
+    width:99%;
+    height:48px;
+    position: absolute;
+    // left:0;
+    top:0;
+    z-index:-1;
+    background:purple;
+  }
+}
 
+.fade-enter-from,.fade-leave-to{
+  transform: translateX(-100%);
+}
+
+.fade-enter-active,.fade-leave-active {
+  transition: all 2s;
+}
 </style>
--- src/App.vue
+++ src/App.vue
@@ -5,55 +5,10 @@
   <div style="width:200px;">
     <pied-upload></pied-upload>
   </div>
-
-  
-  <button @click="handle">dian</button>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const value = ref('1')
-const value1 = ref('2')
-const list = ref([
-  {
-    value: 1,
-    label: 'option1'
-  },
-  {
-    value: 2,
-    label: 'option2'
-  },
-  {
-    value: 3,
-    label: 'option3'
-  },
-  {
-    value: 4,
-    label: 'option4'
-  },
-  {
-    value: 5,
-    label: 'option5'
-  },
-  {
-    value: 6,
-    label: 'option6'
-  },
-  {
-    value: 7,
-    label: 'option7'
-  },
-  {
-    value: 8,
-    label: 'option8'
-  }
-])
-const handle = () => {
-  console.log(value.value)
-}
-const change = (item) => {
-  console.log(item)
-}
+
 </script>
 
 <style scoped>
